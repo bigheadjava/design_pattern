12ce3d8e4b3d91326a40685ab4ceefd9111d936d
yinchu <javayinchu2013@163.com>
2019-06-28 12:18:19 +0800

    懒汉式单例模式

---
+++ src/design_pattern/singleton/lazy/LazySingleton.java
@@ -0,0 +1,30 @@
+package design_pattern.singleton.lazy;
+
+/**
+ * 懒汉式单例模式
+ * 
+ * @author 程序猿大头
+ *
+ */
+public class LazySingleton {
+	private static LazySingleton ls;
+
+	/**
+	 * 构造方法私有
+	 */
+	private LazySingleton() {
+
+	}
+	
+	/**
+	 * 提供给外界获取实例的方法
+	 * 
+	 * @return
+	 */
+	public static LazySingleton getInstance() {
+		if (ls == null) {
+			return new LazySingleton();
+		}
+		return ls;
+	}
+}
