2eaaaeb36f376e8242762a34afa1610879005217
yinchu <javayinchu2013@163.com>
2019-09-11 22:14:48 +0800

    updated

--- src/design_pattern/proxy/game/v4/Demo.java
+++ src/design_pattern/proxy/game/v4/Demo.java
@@ -4,8 +4,9 @@ public class Demo {
 
 	public static void main(String[] args) throws Exception {
 		IGamePlayer player = new GamePlayer("孙悟空"); 
-		//IGamePlayer proxy =  new GamePlayerProxy(player);
-		IGamePlayer proxy = player.getProxy();
+		
+		
+		IGamePlayer proxy =  new GamePlayerProxy(player);
 		proxy.login("monkey", "password");
 		proxy.killBoss();
 		proxy.upgrade();
